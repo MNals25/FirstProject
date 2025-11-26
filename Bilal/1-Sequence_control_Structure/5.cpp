a4a51bf10bbc6e365a19c8770658cea42f2fd364
m25edu <mohammed25.edu@gmail.com>
2025-11-26 23:17:31 +0300

    Calculate the Pythagorean theorem to Find the Length of C according to the law C= sqrt((a*a)+(b*b))

---
+++ Bilal/1-Sequence_control_Structure/5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<"Calculate the Pythagorean theorem to Find the Length of C according to the law C= sqrt((a*a)+(b*b))\n\n";
+    float a,b,c;
+    cout<<"Enter the value a: ";
+    cin>>a;
+    cout<<"Enter the value b: ";
+    cin>>b;
+    c=sqrt((a*a)+(b*b));
+    cout<<"the Pythagorean theorem to Find the Length of C according to the law is: "<<c<<endl;
+
+
+
+}
