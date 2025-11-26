cd9cac4e8950213fb20a0c8a1ec02a663ab0dfbe
m25edu <mohammed25.edu@gmail.com>
2025-11-26 21:59:06 +0300

    calculate your age (age = current year - birth year

---
+++ Bilal/1-Sequence_control_Structure/2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"calculate your age (age = current year - birth year) \n\n";
+    int age,CurrentYear,BirthYear;
+    cout<<"Enter the Current Year:";
+    cin>>CurrentYear;
+    cout<<"Birth Year:";
+    cin>>BirthYear;
+    age=CurrentYear-BirthYear;
+    cout<<"Your age is "<<age<<endl;
+    
+    
+    
+    }
---
+++ Bilal/1-Sequence_control_Structure/3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"convert from minute to second s=(m*60) \n\n";
+    float m,s;
+    cout<<"Enter the Minute:";
+    cin>>m;
+    s=m*60;
+    cout<<"The second is:"<<s<<endl;
+    
+    
+    }
