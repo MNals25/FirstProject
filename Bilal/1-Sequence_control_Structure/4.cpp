21e82c0224c9fbd0840a3ba2eafe03020743f19b
m25edu <mohammed25.edu@gmail.com>
2025-11-26 22:41:02 +0300

    Calculate and print Total price With Tax = 0.15 Total = Price+(Price*0.15)

---
+++ Bilal/1-Sequence_control_Structure/4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Calculate and print Total price With Tax = 0.15\nTotal = Price+(Price*0.15)\n\n";
+    float price,Total;
+    cout<<"Enter the price: ";
+    cin>>price;
+    Total=price+(price*0.15);
+    cout<<"The total after tax is "<<Total<<"$"<<endl;
+
+
+
+
+
+
+
+}
