1b400eb4e432c6d6ee28d6caf632196f452fa4f7
m25edu <mohammed25.edu@gmail.com>
2025-11-26 23:43:15 +0300

    Calculate average of three numbers.

---
+++ Bilal/1-Sequence_control_Structure/7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    cout<<"Calculate average of three numbers.\n\n";
+    float num1,num2,num3,avg;
+    cout<<"Enter the number 1: ";
+    cin>>num1;
+    cout<<"Enter the number 2: ";
+    cin>>num2;
+    cout<<"Enter the number 3: ";
+    cin>>num3;
+    avg=(num1+num2+num3)/3;
+    cout<<"The average of three number is: "<<avg;
+    
+
+}
