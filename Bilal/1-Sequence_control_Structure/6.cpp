68f8848136ca92f75c99b1665224251302de0973
m25edu <mohammed25.edu@gmail.com>
2025-11-26 23:30:03 +0300

    Calculate the area of a circle for a given value of radius (r), where area of circle Area=p*(r*r)

---
+++ Bilal/1-Sequence_control_Structure/6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r,Area;
+    const float p=3.14;
+    cout<<"Calculate the area of a circle for a given value of radius (r), where area of circle Area=p*(r*r)\n\n";
+    cout<<"Enter the radius: ";
+    cin>>r;
+    Area=p*(r*r);
+    cout<<"The area of a circle with a radius of "<<r<<" is equal to : "<<Area<<endl;
+
+
+
+
+
+
+}
