48a32049206b42e46174b7892f83c1c0790c714b
m25edu <mohammed25.edu@gmail.com>
2025-11-28 02:16:41 +0300

    Print the even numbers from 0 to 10

---
+++ Bilal/3-Repetition_control_Structure/2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Print the even numbers from 0 to 10\n";
+    for(int i=0;i<=10;i++){
+        if(i%2==0){cout<<i<<endl;}
+    }
+    return 0;
+}
