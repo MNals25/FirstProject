f88ccc01be6fb1781612828109703106392df742
m25edu <mohammed25.edu@gmail.com>
2025-11-28 01:48:43 +0300

    Check whether a number is even or odd:

---
+++ Bilal/2-Branching_selection_control_Structure/4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Check whether a number is even or odd: ";
+    int num;
+    cin>>num;
+    if(num%2==0){cout<<"The number is even";}
+    else{cout<<"The number is odd";}
+    
+    
+    return 0;
+}
