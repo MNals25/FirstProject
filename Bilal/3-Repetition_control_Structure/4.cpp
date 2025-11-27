fa5cdbafde34e4825487fdae615f616c7302449a
m25edu <mohammed25.edu@gmail.com>
2025-11-28 02:31:35 +0300

    Find the summation of first 10 numbers. (from 1 to 50)

---
+++ Bilal/3-Repetition_control_Structure/4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Find the summation of first 10 numbers. (from 1 to 50) \n";
+    int sum=0;
+    for(int i=1;i<=50;i++){
+        if(i<=10){sum+=i;}
+        }
+    cout<<"is equal: "<<sum;
+    return 0;
+}
