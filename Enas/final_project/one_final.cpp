bd984431df686964798a7d03e9340c10eb285688
m25edu <mohammed25.edu@gmail.com>
2025-11-30 20:38:28 +0300

    change file

---
+++ Enas/final_project/one_final.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Write a program in C++ to divide two numbers and print on the screen?\n";
+    cout<<"Divide two number and print\n";
+    float num1=30,num2=10;
+    cout<<"The quotient of 30 and 10 is "<<num1/num2;
+}
