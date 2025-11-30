206577cdc3e84147fdff6718402e0d49ccd51d46
m25edu <mohammed25.edu@gmail.com>
2025-11-30 22:25:46 +0300

    add the file

---
+++ Enas/Assignmants/Assignmants_lab2/Assignmants1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Write a C++ program to produce an output that looks like the following.\n";
+    cout<<"My Name is Mohammed Alsalehi\n";
+}
